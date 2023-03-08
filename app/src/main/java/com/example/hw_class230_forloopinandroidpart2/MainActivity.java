c56b844b9a6788755f80989d3433d0875b10d533
Raian-Siddique <raian0197@gmail.com>
2023-03-08 02:10:02 +0100

    initial commit

---
+++ app/src/main/java/com/example/hw_class230_forloopinandroidpart2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hw_class230_forloopinandroidpart2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
